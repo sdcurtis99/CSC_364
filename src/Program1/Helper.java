95d320caf0fd7fe6a306cf20d24b2644a9b74507
shannoncurtis <shannoncurtis40lb@gmail.com>
2026-02-25 13:09:18 -0800

    Added ideas for easier hashmap implementation

--- src/Program1/Helper.java
+++ src/Program1/Helper.java
@@ -1,6 +1,30 @@
 package Program1;
 
+import java.util.Timer;
+
+
 public class Helper {
+
+    /*
+
+    Better design pattern would be to use a single Map aka
+    JobMap<String/Integer, JobInfo(object)>
+    Calling Ex:
+            <JobInfo.getID(), JobInfo>
+    Should create a new class JobInfo for ease
+
+    Need to override the to string, hashcode, and equals depending on implementation for HashMap
+
+    Methods add, remove, get.
+    Once you have an object use the api of JobInfo class to make changes
+    Do calculations etc
+
+    Look into duplicates, error checking, etc in hashmaps
+
+     */
+
+
+
     /*private final ConcurrentHashMap<Integer, Long> startTimes = new ConcurrentHashMap<>();
     private final ConcurrentHashMap<Integer, Long> endTimes   = new ConcurrentHashMap<>();
     private final ConcurrentHashMap<Integer, Integer> results = new ConcurrentHashMap<>();
